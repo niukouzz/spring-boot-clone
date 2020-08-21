91bfeafeefb80ae3faeeec86851889ac3b90b48a
songweidong <18510894615@163.com>
2020-08-21 11:18:36 +0800

    commitAAAAA

--- src/main/java/com/song/springboot/controller/HomeController.java
+++ src/main/java/com/song/springboot/controller/HomeController.java
@@ -1,9 +1,30 @@
 package com.song.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * @author King
  * @description api
  * @date 2020/8/20
  **/
+@RestController
 public class HomeController {
+
+    @Value("${hschefu}")
+    private String name;
+    @Value("${hschefu1}")
+    private String name1;
+
+    @RequestMapping(value = "view02", method = RequestMethod.GET)
+    public ModelAndView view (){
+        System.out.println(1111);
+        ModelAndView modelAndView = new ModelAndView("view");
+        System.out.printf(name);
+        modelAndView.addObject("name",name1);
+        return modelAndView;
+    }
 }
